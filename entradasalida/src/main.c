c91509554a877b24398134a06f8270938be0e998
TomasLVUTN <tlaurenzanovinciguerra@frba.utn.edu.ar>
2024-05-03 18:36:10 +0000

    Se manda a kernel nombre y tipo

--- entradasalida/src/main.c
+++ entradasalida/src/main.c
@@ -1,7 +1,9 @@
 #include "../include/main.h"
+
 t_log* logger;
 
 int main(int argc, char* argv[]) {
+     char* nombreDeInterfaz;
     int fd_conexion_kernel = 0;
     int fd_conexion_memoria = 0;
    // t_log* logger;
@@ -13,44 +15,40 @@ int main(int argc, char* argv[]) {
         log_error(logger,"Error al crear conexiones iniciales");
         exit(1);
     }
-    mandarHandshake(logger,fd_conexion_kernel,"MODULO KERNEL","INTERFAZ CUALQUIERA");
+    /*mandarHandshake(logger,fd_conexion_kernel,"MODULO KERNEL","INTERFAZ CUALQUIERA");
     mandarHandshake(logger,fd_conexion_memoria,"MODULO MEMORIA","INTERFAZ CUALQUIERA");
-    //manejarConexion(logger,&fd_conexion_kernel,&fd_conexion_memoria);
-    terminar_programa(logger,config,&fd_conexion_kernel,&fd_conexion_memoria);
-      //pedir nombre
-      //scanf
-    //agregar iniciar interfaz
-  
+    manejarConexion(logger,&fd_conexion_kernel,&fd_conexion_memoria);*/
+    
+   
+    nombreDeInterfaz = readline(">");
+    printf("%s\n", nombreDeInterfaz);
+    log_info(logger,"El tipo de interfaz recibida es: %s", nombreDeInterfaz);
+    iniciarInterfaz(nombreDeInterfaz, config, fd_conexion_kernel );
+    free(nombreDeInterfaz);
 
-   return 0;
-
-//***interfaz generica***//
-
-//recibo peticion de kernel??? LA PETICION ESTA EN EL CONFIG O EN EL KERNEL, O KERNEL ESCRIBE EL CONFIG?
-//de kernel recibo cantidad de unidades de trabajo
-//la interfaz generica solo recibe IO_GEN_SLEEP
-//leo config
-//miro el tipo de interfaz, este caso, generica
-//
-
+    //terminar_programa(logger,config,&fd_conexion_kernel,&fd_conexion_memoria);
+    return 0;
 }
 
-void iniciarInterfaz(char nombre,  t_config* config, int fd_conexion_kernel )// o hacerlo global? deberiamos agregar el fd de kernel, despues tambien habria que agregar el de la memoria
+void iniciarInterfaz(char* nombreDeInterfaz,  t_config* config, int fd_conexion_kernel )// o hacerlo global? deberiamos agregar el fd de kernel, despues tambien habria que agregar el de la memoria
 {
-    
-char* tipoDeInterfaz;
-//int* tiempoDeUnidadDeTrabajo;
+    char* tipoYnombreDeInterfaz = string_new();
 
-tipoDeInterfaz = config_get_string_value(config, "TIPO_INTERFAZ");
-//tiempoDeUnidadDeTrabajo = config_get_int_value(config, "TIEMPO_UNIDAD_TRABAJO");
-
-if (!(strcmp(tipoDeInterfaz, "GENERICO")))
-{
-    //log_info(logger,"La interfaz recibida es: GENERICA");
-    interfazGenerica(config, fd_conexion_kernel);
-    
-
-}
+    char* tipoDeInterfaz;
+    //int* tiempoDeUnidadDeTrabajo;
+    tipoDeInterfaz = config_get_string_value(config, "TIPO_INTERFAZ");
+    //tiempoDeUnidadDeTrabajo = config_get_int_value(config, "TIEMPO_UNIDAD_TRABAJO");
+    string_append(&tipoYnombreDeInterfaz, tipoDeInterfaz);
+    string_append(&tipoYnombreDeInterfaz, "-");
+    string_append(&tipoYnombreDeInterfaz, nombreDeInterfaz);
+    mandarHandshake(logger,fd_conexion_kernel,"MODULO KERNEL",tipoYnombreDeInterfaz);
+    free(tipoYnombreDeInterfaz);
+    //mandarHandshake(logger,fd_conexion_memoria,"MODULO MEMORIA","INTERFAZ CUALQUIERA");
+        if (!(strcmp(tipoDeInterfaz, "GENERICO")))
+            {
+                log_info(logger,"La interfaz recibida es: GENERICA");
+                interfazGenerica(config, fd_conexion_kernel);
+            }
 }
 
 void interfazGenerica( t_config* config, int fd_conexion_kernel)//pasa el config para no leer cada vez que inicia una interfaz todo completo
@@ -58,14 +56,17 @@ void interfazGenerica( t_config* config, int fd_conexion_kernel)//pasa el config
     int tiempoDeUnidadDeTrabajo;
     int cantidad_unidad_trabajo;
     tiempoDeUnidadDeTrabajo = config_get_int_value(config, "TIEMPO_UNIDAD_TRABAJO");
-    
+    while(1)
+        {
     recv(fd_conexion_kernel,&cantidad_unidad_trabajo,sizeof(int),MSG_WAITALL);
         int resultado = cantidad_unidad_trabajo * tiempoDeUnidadDeTrabajo; //cantidad viene de kernel
         usleep(resultado);
         int finalizado = 4; // aca deberia ir el valor del ENUM INTERFAZLISTA
         log_info(logger,"Finalizo el tiempo en espera de la interfaz generica");
         send(fd_conexion_kernel,&finalizado,sizeof(int),0);
-        
+        }
 
 
 }
+
+
