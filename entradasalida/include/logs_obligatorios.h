5b38bff5ff75a43b5149eb2716af4c7835e7ed17
Roberto <rrodasgaleano@frba.utn.edu.ar>
2024-06-28 01:19:06 +0000

    aniadidos logs obligatorios dialFS

--- entradasalida/include/logs_obligatorios.h
+++ entradasalida/include/logs_obligatorios.h
@@ -9,7 +9,13 @@
 
 /**LOGS_OBLIGATORIOS**/
 void logear_truncate(t_log* logger, int pid, char* nombre_archivo, int tamanio_truncar);
-
+void logear_creacion_archivo(t_log* logger, int pid, char* nombre_archivo);
+void logear_eliminacion_archivo(t_log* logger, int pid, char* nombre_archivo);
+void logear_lectura_archivo(t_log* logger, int pid, char* nombre_archivo, int tam_leer, int puntero_archivo);
+void logear_escritura_archivo(t_log* logger, int pid, char* nombre_archivo, int tam_escribir, int puntero_archivo);
+void logear_inicio_compactacion(t_log* logger, int pid);
+void logear_fin_compactacion(t_log* logger, int pid);
+void logear_operacion_dialFS(t_log* logger, int pid, int operacion_realizar);
 
 /**METADATAS**/
 //void modificar_metadata(t_config* config_metadata, int tamanio, int bloque_inicial);
--- entradasalida/src/logs_obligatorios.c
+++ entradasalida/src/logs_obligatorios.c
@@ -4,6 +4,71 @@ void logear_truncate(t_log* logger, int pid, char* nombre_archivo, int tamanio_t
 {
     log_info(logger,"DialFS - Truncar Archivo: \"PID: %d - Truncar Archivo: %s - Tamaño: %d\"",pid,nombre_archivo,tamanio_truncar);
 }
+
+void logear_creacion_archivo(t_log* logger, int pid, char* nombre_archivo)
+{
+    log_info(logger,"DialFS - Crear Archivo: \"PID: %d - Crear Archivo: %s\"",pid,nombre_archivo);
+}
+
+void logear_eliminacion_archivo(t_log* logger, int pid, char* nombre_archivo)
+{
+    log_info(logger,"DialFS - Eliminar Archivo: \"PID: %d - Eliminar Archivo: %s\"",pid,nombre_archivo);
+}
+
+void logear_lectura_archivo(t_log* logger, int pid, char* nombre_archivo, int tam_leer, int puntero_archivo)
+{
+    log_info(logger,"DialFS - Leer Archivo: \"PID: %d - Leer Archivo: %s - Tamaño a Leer: %d - Puntero Archivo: %d\"",pid,nombre_archivo,tam_leer,puntero_archivo);
+}
+
+void logear_escritura_archivo(t_log* logger, int pid, char* nombre_archivo, int tam_escribir, int puntero_archivo)
+{
+    log_info(logger,"DialFS - Escribir Archivo: \"PID: %d - Escribir Archivo: %s - Tamaño a Escribir: %d - Puntero Archivo: %d\"",pid,nombre_archivo,tam_escribir,puntero_archivo);
+}
+
+void logear_inicio_compactacion(t_log* logger, int pid)
+{
+     log_info(logger,"DialFS - Inicio Compactacion: \"PID: %d - Inicio Compactacion.\"",pid);
+}
+
+void logear_fin_compactacion(t_log* logger, int pid)
+{
+    log_info(logger,"DialFS - Fin Compactacion: \"PID: %d - Fin Compactacion.\"",pid);
+}
+
+void logear_operacion_dialFS(t_log* logger, int pid, int operacion_realizar)
+{
+    switch(operacion_realizar)
+    {
+        case IO_FS_CREATE:
+        {
+            log_info(logger,"DialFS - Operacion: \"PID: %d - Operacion: IO_FS_CREATE\"",pid);
+        }
+        case IO_FS_DELETE:
+        {
+            log_info(logger,"DialFS - Operacion: \"PID: %d - Operacion: IO_FS_DELETE\"",pid);
+        }
+        case IO_FS_TRUNCATE:
+        {
+            log_info(logger,"DialFS - Operacion: \"PID: %d - Operacion: IO_FS_TRUNCATE\"",pid);
+        }
+        case IO_FS_WRITE:
+        {
+            log_info(logger,"DialFS - Operacion: \"PID: %d - Operacion: IO_FS_WRITE\"",pid);
+        }
+        case IO_FS_READ:
+        {
+            log_info(logger,"DialFS - Operacion: \"PID: %d - Operacion: IO_FS_READ\"",pid);
+        }
+        default:
+        {
+            log_warning(logger,"OPERACION SOLICITADA AL dialFS DESCONOCIDA");
+        }
+    }
+}
+
+
+
+
 /*
 void modificar_metadata(t_config* config_metadata, int tamanio, int bloque_inicial)
 {   
